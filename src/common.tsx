45329a0a03e37692f2416ec1c8eebc161840d966
yogesh.r@abyantra.com+1-at-565110426326 <yogesh.r@abyantra.com+1-at-565110426326>
2024-08-14 13:03:02 +0530

    Dashboard and add modal base

--- src/common.tsx
+++ src/common.tsx
@@ -1,3 +1,71 @@
-export const serverPath={
-    authserver:"http://172.104.56.206:9077/auth/"
-}
+import { createTheme } from '@mui/material/styles';
+
+export const serverPath = { authserver: "http://172.104.56.206:9077/auth/" }
+
+export const sd = (css: string) => (getComputedStyle(document.documentElement).getPropertyValue(css).trim())
+
+export const appTheme = createTheme({
+    palette: { primary: { main: sd('--text-color-hover') } },
+    typography: { fontFamily: sd('--text-font') },
+    components: {
+        MuiButton: {
+            styleOverrides: {
+                root: {
+                    borderRadius: sd('--button-bradius'), color: sd('--text-color-default'), backgroundColor: sd('--button-bgcolor-active-brand'), textTransform: 'none',
+                    '&:hover': { color: sd('--text-color-hover'), backgroundColor: sd('--button-bgcolor-hover-brand') },
+                    '&.Mui-disabled': { color: sd('--text-color-disabled'), backgroundColor: sd('--button-bgcolor-disabled') }
+                }
+            }
+        },
+        MuiCard: {
+            styleOverrides: {
+                root: {
+                    padding: sd('--box-padding'), margin: sd('--box-margin'), borderRadius: sd('--box-bradius'),
+                    backgroundColor: sd('--box-bgcolor-default')
+                }
+            }
+        },
+        MuiDialog: {
+            styleOverrides: {
+                root: {
+                    padding: sd('--box-padding'), margin: sd('--box-margin'), borderRadius: sd('--box-bradius'),
+                    backgroundColor: sd('--box-bgcolor-default')
+                }
+            }
+        },
+        MuiDialogTitle: {
+            styleOverrides: {
+                root: {
+                    borderBottom:'1px solid black'
+                }
+            }
+        },
+        MuiDialogActions: {
+            styleOverrides: {
+                root: {
+                    borderTop:'1px solid black'
+                }
+            }
+        },        
+        MuiTextField: {
+            styleOverrides: {
+                root: {
+                    '& .MuiOutlinedInput-root': {
+                        '& fieldset': { borderColor: sd('--text-color-disabled') },
+                        '&.Mui-focused fieldset': { borderColor: sd('--text-color-special'), },
+                    }
+                }
+            }
+        },
+        MuiLink: {
+            styleOverrides: {
+                root: {
+                    color: sd('--text-color-special'),
+                    cursor: sd('--text-styles-pointer'),
+                    '&:hover': { color: sd('--text-color-hover') }
+                }
+            },
+            defaultProps: { underline: 'hover' }
+        }
+    }
+})
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material/styles';
 import './web.css';
+import { appTheme } from './common';
+import Login from './components/LoginPage/Login';
+import Dashboard from './components/Dashboard';
 
-export const sd = (css: string) => (getComputedStyle(document.documentElement).getPropertyValue(css).trim());
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={appTheme}>
+      <Router><Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Dashboard />} />
+      </Routes></Router>
+    </ThemeProvider>
   </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
