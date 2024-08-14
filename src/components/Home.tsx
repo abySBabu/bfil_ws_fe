bc65f8c8519dd645d3bcd97623229fd7b075a189
yogesh.r@abyantra.com+1-at-565110426326 <yogesh.r@abyantra.com+1-at-565110426326>
2024-08-14 15:47:42 +0530

    Header Styling

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import {
-    Box, Drawer, Button, List, Divider, ListItem, ListItemButton, ListItemText, Typography,
-    AppBar, Toolbar, IconButton
+    Box, Drawer, List, Divider, ListItem, ListItemButton, ListItemText, Typography, Toolbar, AppBar
 } from '@mui/material';
 import { Menu, PersonRounded } from '@mui/icons-material/';
+import { sd } from '../common';
 
 export default function Home() {
     const [dIndex, setdIndex] = React.useState(0);
@@ -12,15 +12,14 @@ export default function Home() {
     const sections = ['Dashboard', 'Intervention Approval', 'User Management', 'Role Management'];
 
     return (<Box sx={{ flexGrow: 1 }}>
-        <AppBar component={'header'}><Toolbar sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-            <Button variant='text' startIcon={<Menu onClick={() => setopen(true)} sx={{ color: 'white' }} />}
-                sx={{ color: 'white', background: 'none', '&:hover': { color: 'white', background: 'none', } }}>
-                <Typography variant="h6">{sections[dIndex]}</Typography>
-            </Button>
-
+        <AppBar component={'header'} sx={{ display: sd('--layout-flex'), flexDirection: sd('--layout-xflex'), justifyContent: sd('--align-spaced'), bgcolor: sd('--header-bgcolor'), p: sd('--header-padding') }}>
+            <div style={{ display: sd('--layout-flex'), alignItems: sd('--align-center') }}>
+                <Menu onClick={() => setopen(true)} />
+                <Typography variant="h6" sx={{ ml: 1 }}>{sections[dIndex]}</Typography>
+            </div>
             <Typography variant='h5'>BFIL - WS</Typography>
             <PersonRounded />
-        </Toolbar></AppBar>
+        </AppBar>
         <Drawer open={open} onClose={() => setopen(false)}>
             <Box sx={{ width: 250 }} role="presentation" onClick={() => setopen(false)}>
                 <Typography variant='h6' sx={{ textAlign: 'center', my: '10px' }}>BFIL - Watershed</Typography>
