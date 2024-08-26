7f1cfd68db5b91049c7e34c49b8eaeb89a415aee
yogesh.r@abyantra.com+1-at-565110426326 <yogesh.r@abyantra.com+1-at-565110426326>
2024-08-26 18:01:33 +0530

    Platform agnostic font

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -3,8 +3,8 @@ import { Box, Card, CardContent, CardMedia, Typography } from '@mui/material';
 
 export const Dashboard: React.FC = () => {
     const keys = ['Watershed Area Treated', 'Water Conserved', 'Farmers Impacted', 'Goverment Amount Leveraged']
-    return (<Box sx={{ display: 'flex', flexDirection: 'row', gap: '4px' }}>
-        {keys.map((k) => (<Card sx={{ display: 'flex', height: '20%', width: '24%', bgcolor: 'gray' }} raised>
+    return (<Box sx={{ display: 'flex', flexDirection: 'row', gap: '8px' }}>
+        {keys.map((k) => (<Card sx={{ display: 'flex', height: '160px', width: '24%' }} raised>
             <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
                 <CardContent>
                     <Typography variant='h6'>{k}</Typography>
@@ -12,7 +12,7 @@ export const Dashboard: React.FC = () => {
                 </CardContent>
                 <CardMedia
                     component="img"
-                    sx={{ width: 160 }}
+                    sx={{ width: 80 }}
                     image="/static/images/cards/live-from-space.jpg"
                     alt="Live from space album cover"
                 />
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
+//import './font.css';
 import './web.css';
 import { appTheme } from './common';
 import Login from './components/LoginPage/Login';
-import {Home} from './Home';
+import { Home } from './Home';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
@@ -18,3 +19,8 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     </ThemeProvider>
   </React.StrictMode>
 )
+
+const fontLink = document.createElement('link');
+fontLink.rel = 'stylesheet';
+fontLink.href = 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap';
+document.head.appendChild(fontLink);
