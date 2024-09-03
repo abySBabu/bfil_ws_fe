acf15f377f5816ba143d8a0e83a443565bd916cf
yogesh.r@abyantra.com+1-at-565110426326 <yogesh.r@abyantra.com+1-at-565110426326>
2024-09-03 13:20:54 +0530

    Textfield height update

--- src/components/Watershed/WsActivity.tsx
+++ src/components/Watershed/WsActivity.tsx
@@ -10,7 +10,7 @@ export const WsActivity: React.FC = () => {
     const [selected, setselected] = React.useState(false);
     const [page, setPage] = React.useState(0);
     const rPP = 10;
-    const tHeads: string[] = ['Watershed Location', 'Farmer', 'Intervention', 'Land Type', 'Total Units', 'Water Conserved', 'Funds Spent', 'Funds Source'];
+    const tHeads: string[] = ['Survey No', 'Farmer', 'Activity', 'Total Units', 'Land Type', 'Water Conserved', 'Funds Spent', 'Funds Source'];
 
     return (<Box sx={{ width: '100%' }}>
         <Table>
@@ -53,28 +53,27 @@ export const WsActivity: React.FC = () => {
             <DialogTitle>Activity Name</DialogTitle>
 
             <DialogContent><Grid container spacing={2} sx={{ my: 1 }}>
-                <Grid item xs={12}><Divider component={Typography} textAlign='left'>Intervention Details</Divider></Grid>
-                <Grid item xs={6}><TextField disabled label='Intervention Side' value="Supply" /></Grid>
-                <Grid item xs={6}><TextField disabled label='Intervention Type (Task)' value="Earthen bunding" /></Grid>
                 <Grid item xs={12}><Divider component={Typography} textAlign='left'>Watershed Details</Divider></Grid>
-                <Grid item xs={4}><TextField disabled label='State' value="Karnataka" /></Grid>
-                <Grid item xs={4}><TextField disabled label='District' value="Value" /></Grid>
-                <Grid item xs={4}><TextField disabled label='Taluk' value="Value" /></Grid>
-                <Grid item xs={4}><TextField disabled label='Panchayat' value="Value" /></Grid>
-                <Grid item xs={4}><TextField disabled label='Watershed' value="Value" /></Grid>
-                <Grid item xs={4}><TextField disabled label='Village' value="Value" /></Grid>
-                <Grid item xs={4}><TextField disabled label='Survey No.' value="Value" /></Grid>
+                <Grid item xs={3}><TextField disabled label='State' value="Karnataka" /></Grid>
+                <Grid item xs={3}><TextField disabled label='District' value="Value" /></Grid>
+                <Grid item xs={3}><TextField disabled label='Taluk' value="Value" /></Grid>
+                <Grid item xs={3}><TextField disabled label='Panchayat' value="Value" /></Grid>
+                <Grid item xs={3}><TextField disabled label='Watershed' value="Value" /></Grid>
+                <Grid item xs={3}><TextField disabled label='Village' value="Value" /></Grid>
+                <Grid item xs={3}><TextField disabled label='Survey No.' value="Value" /></Grid>
                 <Grid item xs={12}><Divider component={Typography} textAlign='left'>Farmer Details</Divider></Grid>
-                <Grid item xs={4}><TextField disabled label='Name' value="Name" /></Grid>
-                <Grid item xs={4}><TextField disabled label='Aadhar' value="0000 1111 2222" /></Grid>
-                <Grid item xs={4}><TextField disabled label='Mobile No.' value="0123456789" /></Grid>
+                <Grid item xs={3}><TextField disabled label='Name' value="Name" /></Grid>
+                <Grid item xs={3}><TextField disabled label='Aadhar' value="0000 1111 2222" /></Grid>
+                <Grid item xs={3}><TextField disabled label='Mobile No.' value="0123456789" /></Grid>
                 <Grid item xs={12}><Divider component={Typography} textAlign='left'>Activity Details</Divider></Grid>
-                <Grid item xs={4}><TextField disabled label='Land Type' value="Wet land" /></Grid>
-                <Grid item xs={4}><TextField disabled label='Total Units' value="200 sqft" /></Grid>
-                <Grid item xs={4}><TextField disabled label="Water Conserved" value="40000 litres" /></Grid>
-                <Grid item xs={4}><TextField disabled label="Funds spent" value="2,00,000" /></Grid>
-                <Grid item xs={4}><TextField disabled label="Funds source" value="BFIL" /></Grid>
-                <Grid item xs={4}><TextField disabled label="Image" value="Image with geo-tag" /></Grid>
+                <Grid item xs={3}><TextField disabled label='Intervention Side' value="Supply" /></Grid>
+                <Grid item xs={3}><TextField disabled label='Intervention Type (Task)' value="Earthen bunding" /></Grid>
+                <Grid item xs={3}><TextField disabled label='Total Units' value="200 sqft" /></Grid>
+                <Grid item xs={3}><TextField disabled label='Land Type' value="Wet land" /></Grid>
+                <Grid item xs={3}><TextField disabled label="Water Conserved" value="40000 litres" /></Grid>
+                <Grid item xs={3}><TextField disabled label="Funds spent" value="2,00,000" /></Grid>
+                <Grid item xs={3}><TextField disabled label="Funds source" value="BFIL" /></Grid>
+                <Grid item xs={12}><TextField disabled label="Image" value="Image with geo-tag" /></Grid>
             </Grid></DialogContent>
 
             <DialogActions>
