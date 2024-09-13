f078fc4f2d7dd6c0d40c477c76d4203e5cc6316e
lakshmi <lakshmi.v@abyantra.com-at-565110426326>
2024-09-13 16:15:35 +0530

    Mapping List search field styling changes
    Signed-off-by: lakshmi <lakshmi.v@abyantra.com-at-565110426326>

--- src/components/WatersheMapping/MappingList.tsx
+++ src/components/WatersheMapping/MappingList.tsx
@@ -102,11 +102,11 @@ export default function MappingList() {
         {showAddModal ? <MapAdd show={true} hide={hideAddModal} action='Add' mapList={mapData} /> : null}
         {showEditModal && selectedRow ? <MapEdit show={true} hide={hideEditModal} action='Edit' mapList={mapData} mapDetails={selectedRow} /> : null}
 
-        <Box sx={{ mb: 1, display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
-            <Box sx={{ display: 'flex', gap: 2, flexDirection: isMobile ? 'column' : 'row', alignItems: isMobile ? 'stretch' : null }}>
+        <Box sx={{ display: 'flex', justifyContent: 'flex-end', gap: '4px', mb: 1 }}>
                 <FormControl sx={{ width: '200px' }}>
                     <TextField
                         label="Search"
+                        fullWidth={false}
                         value={searchQuery}
                         onChange={(e) => setSearchQuery(e.target.value)}
                         variant="outlined"
@@ -124,7 +124,6 @@ export default function MappingList() {
                     <Button variant="outlined" onClick={() => { setShowAddModal(true) }} startIcon={<PersonAddIcon />}>
                         Add Mapping
                     </Button>)}
-            </Box >
         </Box>
 
         {filteredData.length > 0 ?
