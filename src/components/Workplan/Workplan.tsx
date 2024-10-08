e43818d18ec2f0f49e2e1fb21c569a1f7202008d
yogesh.r@abyantra.com+1-at-565110426326 <yogesh.r@abyantra.com+1-at-565110426326>
2024-10-08 19:11:13 +0530

    Work plan intervention type update

--- src/components/Workplan/Workplan.tsx
+++ src/components/Workplan/Workplan.tsx
@@ -110,8 +110,8 @@ export const Workplan: React.FC = () => {
     const fetchData = async () => {
         try {
             const resp1 = await listWP(); if (resp1.status === 'success') { setplanList(resp1.data) }
-            const resp2 = await ListInter(); if (resp2.status === 'success') { setintOps(resp2.data) }
-            const resp3 = await ListDonor(); if (resp3.status === 'success') { setintOps(prevIntOps => [...prevIntOps, ...resp3.data]) }
+            const resp2 = await ListInter(); const resp3 = await ListDonor();
+            if (resp2.status === 'success' && resp3.status === 'success') { setintOps([...resp2.data, ...resp3.data]) }
             const resp4 = await ListLand(); if (resp4.status === 'success') { setlandOps(resp4.data) }
             const resp5 = await listWS(); if (resp5.status === 'success') { setwsOps(resp5.data) }
         }
