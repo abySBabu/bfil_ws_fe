bd003c3fde701482a68d0063f66dd27d2da51056
lakshmi <lakshmi.v@abyantra.com-at-565110426326>
2024-10-17 17:39:56 +0530

    Added test portal
    Signed-off-by: lakshmi <lakshmi.v@abyantra.com-at-565110426326>

--- src/common.tsx
+++ src/common.tsx
@@ -8,7 +8,10 @@ import { tpaTheme } from './theme';
 
 export const setTimeoutsecs = 3000;
 export const setAutoHideDurationTimeoutsecs = 3000;
-export const serverPath = { authserver: "https://auth1.abynet.xyz/auth/", bfil: "https://bfilbe.abynet.xyz/bfil/", workFlow: "https://workflow1.abynet.xyz/wf/" }
+//Server
+// export const serverPath = { authserver: "https://auth1.abynet.xyz/auth/", bfil: "https://bfilbe.abynet.xyz/bfil/", workFlow: "https://workflow1.abynet.xyz/wf/" }
+//Testport
+export const serverPath = { authserver: "https://auth1.abynet.xyz/auth/", bfil: "https://bfilbetest.abynet.xyz/bfil/", workFlow: "https://workflow1.abynet.xyz/wf/" }
 export const sd = (css: string) => getComputedStyle(document.documentElement).getPropertyValue(css).trim()
 export const PerChk = (per: string): boolean => {
     const permList = sessionStorage.getItem('permList');
