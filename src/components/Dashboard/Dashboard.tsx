118c9008f8b593d774cc486ac7fdbd02e658a4ef
lakshmi <lakshmi.v@abyantra.com-at-565110426326>
2024-10-28 12:01:36 +0530

    Dashboard and mapEdit customer feedback issues changes
    Signed-off-by: lakshmi <lakshmi.v@abyantra.com-at-565110426326>

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -9,10 +9,11 @@ import { DashKey, DashSupply, DashDemand } from '../../Services/activityService'
 import { useTranslation } from 'react-i18next';
 import EsriMap from '../Map';
 import CircularProgress from '@mui/material/CircularProgress';
+import { ListDemand, ListSupply } from 'src/Services/dashboardService';
 
 const keyCard = { height: '120px', overflow: 'auto', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', /* position: 'relative', */ color: sd('--text-color-special'), bgcolor: sd('--card-bgcolor'), p: '8px' }
 
-const ActCard: React.FC<{ activity: string, value: number, unit: string }> = ({ activity, value, unit }) => (
+const ActCard: React.FC<{ activity: string, value: number | string, unit: string }> = ({ activity, value, unit }) => (
     <Grid item xs={6} lg={3}>
         <Card sx={{ height: '85px', overflow: 'auto', borderRadius: sd('--card-bradius'), color: sd('--text-color-special'), bgcolor: sd('--card-bgcolor') }}>
             <CardContent sx={{ textAlign: 'center' }}>
@@ -30,10 +31,22 @@ export const Dashboard: React.FC = () => {
     const [keyList, setkeyList] = React.useState<{ [key: string]: string }>({});
     const [supplyList, setsupplyList] = React.useState<{ [key: string]: { [unit: string]: number } }>({});
     const [demandList, setdemandList] = React.useState<{ [key: string]: { [unit: string]: number } }>({});
+    const [expectedSupplyActivities, setExpectedSupplyActivities] = React.useState<string[]>([]);
+    const [expectedDemandActivities, setExpectedDemandActivities] = React.useState<string[]>([]);
 
     React.useEffect(() => {
         const fetchData = async () => {
             try {
+                const Supplyresp = await ListSupply();
+                if (Supplyresp) {
+                    const activities = Supplyresp.data.map((item: any) => item.activityName);
+                    setExpectedSupplyActivities(activities);
+                }
+                const Demandresp = await ListDemand();
+                if (Demandresp) {
+                    const activities = Demandresp.data.map((item: any) => item.activityName);
+                    setExpectedDemandActivities(activities);
+                }
                 const resp1 = await DashKey();
                 if (resp1) {
                     setkeyList(resp1);
@@ -107,23 +120,21 @@ export const Dashboard: React.FC = () => {
                         <Grid item xs={12} sx={{ mt: 1 }}><Typography variant='h6' fontWeight='bold' sx={{ ml: 1, color: sd('--text-color-special') }}>{t("p_Dashboard.ss_SupplySideInterventions_Header_Text")}</Typography> </Grid>
                         <Grid item xs={12} md={8}>
                             <Grid container spacing={1}>
-                                {
-                                    Object.entries(supplyList)?.map(([activity, data], i) => {
-                                        const [unit, value] = Object.entries(data)[0];
-                                        return (
-                                            <ActCard key={i} activity={activity} value={value} unit={unit} />
-                                        );
-                                    })
-                                }
+                                {expectedSupplyActivities.map((activity, i) => {
+                                    const data = supplyList[activity];
+                                    const [unit, value] = data ? Object.entries(data)[0] : ["N/A", ""];
+                                    return (
+                                        <ActCard key={i} activity={activity} value={value} unit={unit} />
+                                    );
+                                })}
                                 <Grid item xs={12} sx={{ mt: 1 }}><Typography variant='h6' fontWeight='bold' sx={{ ml: 1, color: sd('--text-color-special') }}>{t("p_Dashboard.ss_DemandSideInterventions_Header_Text")}</Typography></Grid>
-                                {
-                                    Object.entries(demandList)?.map(([activity, data], i) => {
-                                        const [unit, value] = Object.entries(data)[0];
-                                        return (
-                                            <ActCard key={i} activity={activity} value={value} unit={unit} />
-                                        );
-                                    })
-                                }
+                                {expectedDemandActivities.map((activity, i) => {
+                                    const data = demandList[activity];
+                                    const [unit, value] = data ? Object.entries(data)[0] : ["N/A", ""];
+                                    return (
+                                        <ActCard key={i} activity={activity} value={value} unit={unit} />
+                                    );
+                                })}
                             </Grid>
                         </Grid>
                         <Grid item xs={12} md={4}>
