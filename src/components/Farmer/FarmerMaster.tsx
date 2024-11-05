68310e3a7b44d376c9435e64c8fdcf92631d02d9
lakshmi <lakshmi.v@abyantra.com-at-565110426326>
2024-11-05 17:57:11 +0530

    Naming changes completed
    Signed-off-by: lakshmi <lakshmi.v@abyantra.com-at-565110426326>

--- src/components/Farmer/FarmerMaster.tsx
+++ src/components/Farmer/FarmerMaster.tsx
@@ -308,7 +308,7 @@ export const FarmerMaster: React.FC = () => {
                     </Table></TableContainer>}
 
                 <Dialog open={addM || editM}>
-                    <DialogTitle>{addM ? t("p_Beneficiary_Master.Add_Beneficiary_Link.Add_Beneficiary_Popup.Add_Mapping_Label") : editM ? 'Edit Beneficiary' : ''}</DialogTitle>
+                    <DialogTitle>{addM ? t("p_Beneficiary_Master.Add_Beneficiary_Link.Add_Beneficiary_Popup.Add_Mapping_Label") : editM ? t("p_Beneficiary_Master.ss_BeneficiaryList.Action.Action_Tooltip.Edit_Tooltip.Edit_Beneficiary_Popup.Edit_Beneficiary_Label") : ''}</DialogTitle>
 
                     <DialogContent><Grid container spacing={1} sx={{ my: 1 }}>
                         <Grid item xs={12} sm={6}>
