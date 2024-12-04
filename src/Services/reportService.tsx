883b69885afa462044e775295ff6a52ec4023964
vasanthihari <vasanthiharikrish@gmail.com>
2024-12-04 11:21:23 -0800

    watershed report based on userId

--- src/Services/reportService.tsx
+++ src/Services/reportService.tsx
@@ -13,9 +13,9 @@ export async function donerReport(financialYear: any) {
     catch (error) { console.error(error); throw error; }
 }
 
-export async function watershedReport(financialYear:any) {
+export async function watershedReport(financialYear:any,userId:number) {
     const configs = {
-        url: serverPath.bfil + `reports/getWatershedReport/${financialYear}`,
+        url: serverPath.bfil + `reports/getWatershedReport/${financialYear}/${userId}`,
         method: 'GET',
         headers: {
             Authorization: `Bearer ${sessionStorage.getItem("token")}`,
@@ -24,4 +24,5 @@ export async function watershedReport(financialYear:any) {
     try { const response = await axios(configs); return response.data; }
     catch (error) { console.error(error); throw error; }
 }
+
 
