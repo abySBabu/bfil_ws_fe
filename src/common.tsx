534c1c2d8e82e992007e30b6aa3469fa1a9367fa
lakshmi <lakshmi.v@abyantra.com-at-565110426326>
2024-12-26 11:50:33 +0530

    Changes in common file
    Signed-off-by: lakshmi <lakshmi.v@abyantra.com-at-565110426326>

--- src/common.tsx
+++ src/common.tsx
@@ -15,9 +15,9 @@ export const setAutoHideDurationTimeoutsecs = 3000;
 // export const serverPath = { authserver: "https://auth1.abynet.xyz/auth/", bfil: "https://bfilbe.abynet.xyz/bfil/", workFlow: "https://workflow1.abynet.xyz/wf/",companyID:"151" }
 //Testport
 
-export const serverPath = { authserver: "http://localhost:8086/auth/", bfil: "http://localhost:8080/bfil/", workFlow: "http://localhost:8083/wf/", companyID: "137"  }
+// export const serverPath = { authserver: "http://localhost:8086/auth/", bfil: "http://localhost:8080/bfil/", workFlow: "http://localhost:8083/wf/", companyID: "137"  }
 
-// export const serverPath = { authserver: "https://auth1.abynet.xyz/auth/", bfil: "https://bfilbedev.abynet.xyz/bfil/", workFlow: "https://workflow1.abynet.xyz/wf/", companyID: "137" }
+export const serverPath = { authserver: "https://auth1.abynet.xyz/auth/", bfil: "https://bfilbedev.abynet.xyz/bfil/", workFlow: "https://workflow1.abynet.xyz/wf/", companyID: "137" }
 export const sd = (css: string) => getComputedStyle(document.documentElement).getPropertyValue(css).trim()
 export const PerChk = (per: string): boolean => {
     const permList = localStorage.getItem('permList');
